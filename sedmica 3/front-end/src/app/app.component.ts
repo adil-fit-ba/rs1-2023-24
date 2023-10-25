853eff1c10dab32a265da2450ffd97dac3961208
Adil <adil@fit.ba>
2023-10-25 10:46:11 +0200

    kraj 3 casas

--- sedmica 3/front-end/src/app/app.component.ts
+++ sedmica 3/front-end/src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
+
+  imena = [];
+
+  brojac=0;
+  ime = "Adil";
+  novoIme="";
+
+  uvecaj(){
+    this.brojac++;
+
+    this.ime = this.ime + "."
+  }
+
+  testirajDugme2(){
+
+    alert("hello FIT...." + this.brojac)
+  }
+
+  izmjenaImena($event: Event) {
+    // @ts-ignore
+    this.ime = $event.target.value
+  }
+
+  isVidljiv() {
+    return this.brojac > 5
+  }
+
+  styleZaDiv() {
+    if (this.brojac == 7) {
+      return {
+        backgroundColor: 'yellow'
+      };
+    }
+    else
+    {
+      return  {
+        backgroundColor:'blue'
+      };
+    }
+  }
+
+  dodajNovoIme() {
+    // @ts-ignore
+    this.imena.push(this.novoIme);
+  }
 }
