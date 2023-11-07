d069d4d450b892eec78153b70568254004278dcc
Adil <adil@fit.ba>
2023-11-07 01:50:56 +0100

    Update ApplicationDbContext.cs

--- sedmica 3/FIT_Api_Examples/FIT_Api_Example/Data/ApplicationDbContext.cs
+++ sedmica 3/FIT_Api_Examples/FIT_Api_Example/Data/ApplicationDbContext.cs
@@ -22,5 +22,16 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(
         DbContextOptions options) : base(options)
     {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.Restrict;
+        }
     }
 }
