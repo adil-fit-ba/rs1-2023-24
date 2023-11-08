5ad0cbded4beaf691336ff30abc2a990f38d1239
Adil <adil@fit.ba>
2023-11-08 09:36:21 +0100

    angular router

--- sedmica 3/front-end/src/app/app.component.ts
+++ sedmica 3/front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,42 +8,15 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit{
 
-  isVidljivoSedmica4 = false;
-  isVidljivoSedmica5 = false;
-  isVidljivoSedmica5PretragaJs = false;
-  isVidljivoSedmica5PretragaBackend = false;
-  isVidljivoSedmica6Edit = false;
+
+  constructor(public router: Router) {
+  }
 
   ngOnInit(): void {
-    // @ts-ignore
-    let params = (new URL(document.location)).searchParams;
-    let putanja = params.get("lokacija");
-
-    this.isVidljivoSedmica4 = false;
-    this.isVidljivoSedmica5 = false;
-    this.isVidljivoSedmica5PretragaJs = false;
-    this.isVidljivoSedmica5PretragaBackend = false;
-    this.isVidljivoSedmica6Edit = false;
-
-    if (putanja == "1")
-      this.isVidljivoSedmica4 = true;
-
-    if (putanja == "2")
-      this.isVidljivoSedmica5 = true;
-
-    if (putanja == "3")
-      this.isVidljivoSedmica5PretragaJs = true;
-
-    if (putanja == "4")
-      this.isVidljivoSedmica5PretragaBackend = true;
-
-    if (putanja == "5")
-      this.isVidljivoSedmica6Edit = true;
   }
 
 
   idi(s: string) {
-    history.pushState("", "", "?"+s);
-    this.ngOnInit();
+    this.router.navigate([s])
   }
 }
--- sedmica 3/front-end/src/app/app.module.ts
+++ sedmica 3/front-end/src/app/app.module.ts
@@ -9,7 +9,7 @@ import {HttpClientModule} from "@angular/common/http";
 import { Sedmica5PretragaJsComponent } from './sedmica5-pretraga-js/sedmica5-pretraga-js.component';
 import { Sedmica5PretragaBackendComponent } from './sedmica5-pretraga-backend/sedmica5-pretraga-backend.component';
 import { Sedmica6EditComponent } from './sedmica6-edit/sedmica6-edit.component';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +22,14 @@ import { Sedmica6EditComponent } from './sedmica6-edit/sedmica6-edit.component';
     imports: [
         BrowserModule,
         FormsModule,
-      HttpClientModule
+      HttpClientModule,
+      RouterModule.forRoot([
+        {path:'sedmica4', component: Sedmica4Component},
+        {path:'sedmica5', component: Sedmica5Component},
+        {path:'sedmica5-js', component: Sedmica5PretragaJsComponent},
+        {path:'sedmica5-backend', component: Sedmica5PretragaBackendComponent},
+        {path:'sedmica6', component: Sedmica6EditComponent},
+      ])
     ],
   providers: [],
   bootstrap: [AppComponent]
