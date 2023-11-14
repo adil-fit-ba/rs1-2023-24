8af2b16d51ebedd3d70bdc865e2bb9c11e9941c3
FIT\adil <adil@fit.ba>
2023-11-14 11:22:48 +0100

    angular router

--- sedmica 3/front-end/src/app/app.component.ts
+++ sedmica 3/front-end/src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   isVidljivoSedmica4 = false;
   isVidljivoSedmica5 = false;
   isVidljivoSedmica5PretragaJs = false;
   isVidljivoSedmica5PretragaBackend = false;
   isVidljivoSedmica6Edit = false;
 
+  ngOnInit(): void {
+    this.ucitajKomponentu();
+  }
+
+  idi(s: string) {
+    //window.location.replace("?q=" + s)
+    window.history.pushState("","","?q=" + s)
+    this.ucitajKomponentu();
+  }
+
+  private ucitajKomponentu() {
+      let s = new URLSearchParams(window.location.search).get('q');
+    this.isVidljivoSedmica4 = false;
+    this.isVidljivoSedmica5 = false;
+    if (s=="sedmica4")
+      this.isVidljivoSedmica4 = true;
+
+    if (s=="sedmica5")
+      this.isVidljivoSedmica5 = true;
+  }
 }
